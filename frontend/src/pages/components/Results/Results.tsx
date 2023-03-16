bca1f5f98edd909bd53b1be63b13cf6c2de7417e
maarioherns21 <maarioherns21@gmail.com>
2023-03-16 13:44:22 -0700

    added shadows

--- frontend/src/pages/components/Results/Results.tsx
+++ frontend/src/pages/components/Results/Results.tsx
@@ -11,20 +11,20 @@ const Results: FC<Props> = ({ question, keywords, onBack, prompt }) => {
     const keywordElements = [];
     for (let i = 0; i < keywords.length; i++) {
       const element = (
-        <div  key={i}  className="bg-teal-200 p-1 text-teal-700 px-2 text-sm rounded-lg">#{keywords[i]}
+        <div  key={i}  className="bg-teal-200 p-1 text-teal-700 px-2 text-sm rounded-lg shadow hover:shadow-xl">#{keywords[i]}
         </div>
       );
       keywordElements.push(element);
     }
   
     const keywordElementsHolder = (
-      <div className="flex flex-wrap gap-2">{keywordElements}</div>
+      <div className="flex flex-wrap gap-2 ">{keywordElements}</div>
     );
   
     const resultSection = (label: string, body: any) => {
       return (
-        <div className=" bg-gray-100 p-1 my-2 rounded-md">
-          <div className="text-white-400 text-md font-bold mb-4">{label}</div>
+        <div className=" bg-gray-50 p-1 my-2 rounded-md shadow hover:shadow-xl">
+          <div className="text-gray-900 focus:outline-none text-md font-bold mb-4">{label}</div>
           <div className="pb-2 justify-center" >{body}</div>
         </div>
       );
